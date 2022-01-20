dddb562aed5c589fd045269bd077e47500edba7c
Lawrence Yoon <lyoon@truechoice.io>
2022-01-20 11:25:48 -0800

    refactor: starting now, deleted previous examples

--- src/utils/test/calculator.test.js
+++ src/utils/test/calculator.test.js
@@ -1,16 +1,3 @@
-// sum, multiply
-const functions = require('../calculator');
-
-describe('sum()', () => {
-  test('adds 2 numbers and returns sum', () => {
-    expect(functions.sum(5, 15)).toBe(20);
-  });
-});
-
-describe('multiply()', () => {
-  test('multiplies 2 numbers and returns result', () => {
-    const numOne = 24;
-    const numTwo = 2;
-    expect(functions.multiply(numOne, numTwo)).toBe(48);
-  });
+describe('Calculator', () => {
+  test('adds 2 numbers, then returns the sum', () => {});
 });
