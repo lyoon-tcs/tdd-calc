ac49af3255574c3deab7f5502e38beb3c418e1b1
Lawrence Yoon <lyoon@truechoice.io>
2022-01-20 13:19:22 -0800

    test: pass first calc sum test

--- src/utils/test/calculator.test.js
+++ src/utils/test/calculator.test.js
@@ -12,15 +12,20 @@ describe('Calculator', () => {
     };
   });
   // addition
-  test('adds 2 numbers, then returns the sum', () => {});
-  test('should only add first two operands and returns sum, ignores the rest', () => {});
-  test('should return operand as sum only if one argument is passed', () => {});
-  test('should return 0 if no operands are passed', () => {});
-  test('should throw error if type is not number', () => {});
-  // subtraction
-  test('should subtract two numbers and return difference', () => {});
-  test('should only subtract first two operands and returns difference, ignores the rest', () => {});
-  test('should return operand as difference only if one argument is passed', () => {});
-  test('should return 0 if no operands are passed', () => {});
-  test('should throw error if type is not number', () => {});
+  test('adds 2 numbers, then returns the sum', () => {
+    const num1 = 4;
+    const num2 = 6;
+    const expected = num1 + num2;
+    expect(calc.sum(num1, num2)).toBe(expected);
+  });
+  // test('should only add first two operands and returns sum, ignores the rest', () => {});
+  // test('should return operand as sum only if one argument is passed', () => {});
+  // test('should return 0 if no operands are passed', () => {});
+  // test('should throw error if type is not number', () => {});
+  // // subtraction
+  // test('should subtract two numbers and return difference', () => {});
+  // test('should only subtract first two operands and returns difference, ignores the rest', () => {});
+  // test('should return operand as difference only if one argument is passed', () => {});
+  // test('should return 0 if no operands are passed', () => {});
+  // test('should throw error if type is not number', () => {});
 });
