49c2705b9e0f93d99d60fe7808fcfa9c8fd2b929
Lawrence Yoon <lyoon@truechoice.io>
2022-01-21 14:31:28 -0800

    feat: update code to reflect passing tests

--- src/utils/calculator.js
+++ src/utils/calculator.js
@@ -2,12 +2,34 @@ class Calculator {
   constructor() {}
 
   getSum(a, b) {
-    return a + b;
+    if (a === undefined && b === undefined) {
+      return 0;
+    } else if (b === undefined) {
+      return a;
+    } else if (typeof a !== 'number' || typeof b !== 'number') {
+      return 'Please use only numbers';
+    } else {
+      return a + b;
+    }
+
+    // if (typeof a !== 'number' && typeof b !== 'number') {
+    //   return 'Please use only numbers';
+    // } else if (a === undefined && b === undefined) {
+    //   return 0;
+    // } else if (b === undefined) {
+    //   return a;
+    // } else {
+    //   return a + b;
+    // }
   }
 
   getDifference(a, b) {
     return a - b;
   }
 }
+
+const calc = new Calculator();
+
+console.log(calc.getSum(2));
 
 module.exports = Calculator;
