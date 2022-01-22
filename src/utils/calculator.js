5966b5abc575d45e7a013110df9a29cc9ed2dba5
Lawrence Yoon <lyoon@truechoice.io>
2022-01-21 16:12:52 -0800

    feat: update code to reflect passing subtraction tests

--- src/utils/calculator.js
+++ src/utils/calculator.js
@@ -1,5 +1,7 @@
 class Calculator {
-  constructor() {}
+  constructor() {
+    this.errorMessage = 'Please use only numbers';
+  }
 
   getSum(a, b) {
     if (typeof a !== 'number' || typeof b !== 'number') {
@@ -9,12 +11,21 @@ class Calculator {
       if (b === undefined) {
         return a;
       }
-      return 'Please use only numbers';
+      return this.errorMessage;
     }
     return a + b;
   }
 
   getDifference(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+      if (a === undefined && b === undefined) {
+        return 0;
+      }
+      if (b === undefined) {
+        return a;
+      }
+      return this.errorMessage;
+    }
     return a - b;
   }
 }
