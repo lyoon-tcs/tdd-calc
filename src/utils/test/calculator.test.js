040967be51324319f5c9716adac7e6d1abfac62f
Lawrence Yoon <lyoon@truechoice.io>
2022-01-24 10:49:55 -0800

    refactor: take in args without use of variables

--- src/utils/test/calculator.test.js
+++ src/utils/test/calculator.test.js
@@ -11,13 +11,7 @@ const calculator = require("../calculator");
 
 describe("Calculator", () => {
   let calc;
-  const num1 = 8;
-  const num2 = 6;
-  const num3 = 4;
-  const firstTwoSum = num1 + num2;
-  const firstTwoDiff = num1 - num2;
-  const firstTwoMultiply = num1 * num2;
-  const errorMessage = "Please use only numbers";
+  const NOT_NUMBER_ERROR = "Please use only numbers";
 
   beforeEach(() => {
     calc = new calculator();
@@ -25,57 +19,57 @@ describe("Calculator", () => {
 
   describe("Addition:", () => {
     test("adds 2 numbers, then returns the sum", () => {
-      expect(calc.getSum(num1, num2)).toBe(firstTwoSum);
+      expect(calc.getSum(5, 1)).toBe(6);
     });
     test("should only add first two operands and returns sum, ignores the rest", () => {
-      expect(calc.getSum(num1, num2, num3)).toBe(firstTwoSum);
+      expect(calc.getSum(5, 1, 3)).toBe(6);
     });
     test("should return operand as sum only if one argument is passed", () => {
-      expect(calc.getSum(num1)).toBe(num1);
+      expect(calc.getSum(5)).toBe(5);
     });
     test("should return 0 if no operands are passed", () => {
       expect(calc.getSum()).toBe(0);
     });
     test("should say error message if type is not a number", () => {
-      expect(calc.getSum(1, "foo")).toBe(errorMessage);
-      expect(calc.getSum("foo", 1)).toBe(errorMessage);
+      expect(calc.getSum(1, "foo")).toBe(NOT_NUMBER_ERROR);
+      expect(calc.getSum("foo", 1)).toBe(NOT_NUMBER_ERROR);
     });
   });
 
   describe("Subtraction:", () => {
     test("should subtract two numbers and return difference", () => {
-      expect(calc.getDifference(num1, num2)).toBe(firstTwoDiff);
+      expect(calc.getDifference(10, 5)).toBe(5);
     });
     test("should only subtract first two operands and returns difference, ignores the rest", () => {
-      expect(calc.getDifference(num1, num2, num3)).toBe(firstTwoDiff);
+      expect(calc.getDifference(10, 3, 2)).toBe(7);
     });
     test("should return operand as difference only if one argument is passed", () => {
-      expect(calc.getDifference(num1)).toBe(num1);
+      expect(calc.getDifference(1)).toBe(1);
     });
     test("should return 0 if no operands are passed", () => {
       expect(calc.getDifference()).toBe(0);
     });
     test("should throw error if type is not number", () => {
-      expect(calc.getDifference(1, "foo")).toBe(errorMessage);
-      expect(calc.getDifference("foo", 1)).toBe(errorMessage);
+      expect(calc.getDifference(1, "foo")).toBe(NOT_NUMBER_ERROR);
+      expect(calc.getDifference("foo", 1)).toBe(NOT_NUMBER_ERROR);
     });
   });
   describe("Multiplication:", () => {
     test("should multiply two numbers and return value", () => {
-      expect(calc.getMultiplication(num1, num2)).toBe(firstTwoMultiply);
+      expect(calc.getMultiplication(2, 3)).toBe(6);
     });
     test("should only multiply first two operands and returns value, ignores the rest", () => {
-      expect(calc.getMultiplication(num1, num2)).toBe(firstTwoMultiply);
+      expect(calc.getMultiplication(5, 2, 3)).toBe(10);
     });
     test("should return operand as value only if one argument is passed", () => {
-      expect(calc.getMultiplication(num1)).toBe(num1);
+      expect(calc.getMultiplication(2)).toBe(2);
     });
     test("should return 0 if no operands are passed", () => {
       expect(calc.getMultiplication()).toBe(0);
     });
     test("should throw error if type is not number", () => {
-      expect(calc.getMultiplication(1, "foo")).toBe(errorMessage);
-      expect(calc.getMultiplication("foo", 1)).toBe(errorMessage);
+      expect(calc.getMultiplication(1, "foo")).toBe(NOT_NUMBER_ERROR);
+      expect(calc.getMultiplication("foo", 1)).toBe(NOT_NUMBER_ERROR);
     });
   });
 });
