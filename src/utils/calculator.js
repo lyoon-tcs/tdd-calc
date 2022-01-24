5d914ce792bdb5734076ea833f53a8541f34617e
Lawrence Yoon <lyoon@truechoice.io>
2022-01-24 10:25:07 -0800

    doc: add TrueChoice header

--- src/utils/calculator.js
+++ src/utils/calculator.js
@@ -1,10 +1,19 @@
+/*
+ * Copyright (c) 2006-present TrueChoice IP Holding Company, Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of TrueChoice IP Holding Company, Inc.
+ * ("Confidential Information").  You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered into with the company.
+ */
+
 class Calculator {
   constructor() {
-    this.errorMessage = 'Please use only numbers';
+    this.errorMessage = "Please use only numbers";
   }
 
   getSum(a, b) {
-    if (typeof a !== 'number' || typeof b !== 'number') {
+    if (typeof a !== "number" || typeof b !== "number") {
       if (a === undefined && b === undefined) {
         return 0;
       }
@@ -17,7 +26,7 @@ class Calculator {
   }
 
   getDifference(a, b) {
-    if (typeof a !== 'number' || typeof b !== 'number') {
+    if (typeof a !== "number" || typeof b !== "number") {
       if (a === undefined && b === undefined) {
         return 0;
       }
--- src/utils/test/calculator.test.js
+++ src/utils/test/calculator.test.js
@@ -1,6 +1,15 @@
-const calculator = require('../calculator');
+/*
+ * Copyright (c) 2006-present TrueChoice IP Holding Company, Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of TrueChoice IP Holding Company, Inc.
+ * ("Confidential Information").  You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered into with the company.
+ */
 
-describe('Calculator', () => {
+const calculator = require("../calculator");
+
+describe("Calculator", () => {
   let calc;
   const num1 = 8;
   const num2 = 6;
@@ -8,65 +17,65 @@ describe('Calculator', () => {
   const firstTwoSum = num1 + num2;
   const firstTwoDiff = num1 - num2;
   const firstTwoMultiply = num1 * num2;
-  const errorMessage = 'Please use only numbers';
+  const errorMessage = "Please use only numbers";
 
   beforeEach(() => {
     calc = new calculator();
   });
 
-  describe('Addition:', () => {
-    test('adds 2 numbers, then returns the sum', () => {
+  describe("Addition:", () => {
+    test("adds 2 numbers, then returns the sum", () => {
       expect(calc.getSum(num1, num2)).toBe(firstTwoSum);
     });
-    test('should only add first two operands and returns sum, ignores the rest', () => {
+    test("should only add first two operands and returns sum, ignores the rest", () => {
       expect(calc.getSum(num1, num2, num3)).toBe(firstTwoSum);
     });
-    test('should return operand as sum only if one argument is passed', () => {
+    test("should return operand as sum only if one argument is passed", () => {
       expect(calc.getSum(num1)).toBe(num1);
     });
-    test('should return 0 if no operands are passed', () => {
+    test("should return 0 if no operands are passed", () => {
       expect(calc.getSum()).toBe(0);
     });
-    test('should say error message if type is not a number', () => {
-      expect(calc.getSum(1, 'foo')).toBe(errorMessage);
-      expect(calc.getSum('foo', 1)).toBe(errorMessage);
+    test("should say error message if type is not a number", () => {
+      expect(calc.getSum(1, "foo")).toBe(errorMessage);
+      expect(calc.getSum("foo", 1)).toBe(errorMessage);
     });
   });
 
-  describe('Subtraction:', () => {
-    test('should subtract two numbers and return difference', () => {
+  describe("Subtraction:", () => {
+    test("should subtract two numbers and return difference", () => {
       expect(calc.getDifference(num1, num2)).toBe(firstTwoDiff);
     });
-    test('should only subtract first two operands and returns difference, ignores the rest', () => {
+    test("should only subtract first two operands and returns difference, ignores the rest", () => {
       expect(calc.getDifference(num1, num2, num3)).toBe(firstTwoDiff);
     });
-    test('should return operand as difference only if one argument is passed', () => {
+    test("should return operand as difference only if one argument is passed", () => {
       expect(calc.getDifference(num1)).toBe(num1);
     });
-    test('should return 0 if no operands are passed', () => {
+    test("should return 0 if no operands are passed", () => {
       expect(calc.getDifference()).toBe(0);
     });
-    test('should throw error if type is not number', () => {
-      expect(calc.getDifference(1, 'foo')).toBe(errorMessage);
-      expect(calc.getDifference('foo', 1)).toBe(errorMessage);
+    test("should throw error if type is not number", () => {
+      expect(calc.getDifference(1, "foo")).toBe(errorMessage);
+      expect(calc.getDifference("foo", 1)).toBe(errorMessage);
     });
   });
-  describe('Multiplication:', () => {
-    test('should multiply two numbers and return value', () => {
+  describe("Multiplication:", () => {
+    test("should multiply two numbers and return value", () => {
       expect(calc.getMultiplication(num1, num2)).toBe(firstTwoMultiply);
     });
-    test('should only multiply first two operands and returns value, ignores the rest', () => {
+    test("should only multiply first two operands and returns value, ignores the rest", () => {
       expect(calc.getMultiplication(num1, num2)).toBe(firstTwoMultiply);
     });
-    test('should return operand as value only if one argument is passed', () => {
+    test("should return operand as value only if one argument is passed", () => {
       expect(calc.getMultiplication(num1)).toBe(num1);
     });
-    test('should return 0 if no operands are passed', () => {
+    test("should return 0 if no operands are passed", () => {
       expect(calc.getMultiplication()).toBe(0);
     });
-    test('should throw error if type is not number', () => {
-      expect(calc.getMultiplication(1, 'foo')).toBe(errorMessage);
-      expect(calc.getMultiplication('foo', 1)).toBe(errorMessage);
+    test("should throw error if type is not number", () => {
+      expect(calc.getMultiplication(1, "foo")).toBe(errorMessage);
+      expect(calc.getMultiplication("foo", 1)).toBe(errorMessage);
     });
   });
 });
