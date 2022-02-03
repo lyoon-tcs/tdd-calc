ca9f9d014fed05fbee730f7a6e91b64daf9a96ed
Lawrence Yoon <lyoon@truechoice.io>
2022-02-03 10:38:17 -0800

    refactor: break it down, work 1 by 1

--- src/utils/test/calculator.test.ts
+++ src/utils/test/calculator.test.ts
@@ -39,72 +39,72 @@ describe('Calculator', () => {
     });
   });
 
-  describe('Subtraction:', () => {
-    test('should subtract two numbers and return difference', () => {
-      expect(calc.getDifference(10, 5)).toBe(5);
-    });
-    test('should only subtract first two operands and returns difference, ignores the rest', () => {
-      expect(calc.getDifference(10, 3, 2)).toBe(7);
-    });
-    test('should return operand as difference only if one argument is passed', () => {
-      expect(calc.getDifference(1)).toBe(1);
-    });
-    test('should return 0 if no operands are passed', () => {
-      expect(calc.getDifference()).toBe(0);
-    });
-    test('should throw error if type is not number', () => {
-      expect(() => {
-        calc.getSubtraction(1, 'foo');
-      }).toThrow();
-      expect(() => {
-        calc.getSubtraction('foo', 1);
-      }).toThrow();
-    });
-  });
+  // describe('Subtraction:', () => {
+  //   test('should subtract two numbers and return difference', () => {
+  //     expect(calc.getDifference(10, 5)).toBe(5);
+  //   });
+  //   test('should only subtract first two operands and returns difference, ignores the rest', () => {
+  //     expect(calc.getDifference(10, 3, 2)).toBe(7);
+  //   });
+  //   test('should return operand as difference only if one argument is passed', () => {
+  //     expect(calc.getDifference(1)).toBe(1);
+  //   });
+  //   test('should return 0 if no operands are passed', () => {
+  //     expect(calc.getDifference()).toBe(0);
+  //   });
+  //   test('should throw error if type is not number', () => {
+  //     expect(() => {
+  //       calc.getSubtraction(1, 'foo');
+  //     }).toThrow();
+  //     expect(() => {
+  //       calc.getSubtraction('foo', 1);
+  //     }).toThrow();
+  //   });
+  // });
 
-  describe('Multiplication:', () => {
-    test('should multiply two numbers and return value', () => {
-      expect(calc.getMultiplication(2, 3)).toBe(6);
-    });
-    test('should only multiply first two operands and returns value, ignores the rest', () => {
-      expect(calc.getMultiplication(5, 2, 3)).toBe(10);
-    });
-    test('should return operand as value only if one argument is passed', () => {
-      expect(calc.getMultiplication(2)).toBe(2);
-    });
-    test('should return 0 if no operands are passed', () => {
-      expect(calc.getMultiplication()).toBe(0);
-    });
-    test('should throw error if type is not number', () => {
-      expect(() => {
-        calc.getMultiplication(1, 'foo');
-      }).toThrow();
-      expect(() => {
-        calc.getMultiplication('foo', 1);
-      }).toThrow();
-    });
-  });
+  // describe('Multiplication:', () => {
+  //   test('should multiply two numbers and return value', () => {
+  //     expect(calc.getMultiplication(2, 3)).toBe(6);
+  //   });
+  //   test('should only multiply first two operands and returns value, ignores the rest', () => {
+  //     expect(calc.getMultiplication(5, 2, 3)).toBe(10);
+  //   });
+  //   test('should return operand as value only if one argument is passed', () => {
+  //     expect(calc.getMultiplication(2)).toBe(2);
+  //   });
+  //   test('should return 0 if no operands are passed', () => {
+  //     expect(calc.getMultiplication()).toBe(0);
+  //   });
+  //   test('should throw error if type is not number', () => {
+  //     expect(() => {
+  //       calc.getMultiplication(1, 'foo');
+  //     }).toThrow();
+  //     expect(() => {
+  //       calc.getMultiplication('foo', 1);
+  //     }).toThrow();
+  //   });
+  // });
 
-  describe('Division:', () => {
-    test('should divide two numbers and return value', () => {
-      expect(calc.getDivision(6, 3)).toBe(2);
-    });
-    test('should only divide first two operands and returns value, ignores the rest', () => {
-      expect(calc.getDivision(12, 3, 2)).toBe(4);
-    });
-    test('should return operand as value only if one argument is passed', () => {
-      expect(calc.getDivision(2)).toBe(2);
-    });
-    test('should return 0 if no operands are passed', () => {
-      expect(calc.getDivision()).toBe(0);
-    });
-    test('should throw error if type is not number', () => {
-      expect(() => {
-        calc.getDivision(1, 'foo');
-      }).toThrow();
-      expect(() => {
-        calc.getDivision('foo', 1);
-      }).toThrow();
-    });
-  });
+  // describe('Division:', () => {
+  //   test('should divide two numbers and return value', () => {
+  //     expect(calc.getDivision(6, 3)).toBe(2);
+  //   });
+  //   test('should only divide first two operands and returns value, ignores the rest', () => {
+  //     expect(calc.getDivision(12, 3, 2)).toBe(4);
+  //   });
+  //   test('should return operand as value only if one argument is passed', () => {
+  //     expect(calc.getDivision(2)).toBe(2);
+  //   });
+  //   test('should return 0 if no operands are passed', () => {
+  //     expect(calc.getDivision()).toBe(0);
+  //   });
+  //   test('should throw error if type is not number', () => {
+  //     expect(() => {
+  //       calc.getDivision(1, 'foo');
+  //     }).toThrow();
+  //     expect(() => {
+  //       calc.getDivision('foo', 1);
+  //     }).toThrow();
+  //   });
+  // });
 });
